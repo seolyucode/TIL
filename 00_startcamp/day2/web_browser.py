2ab7757684148eb0cbfe9ce0e48b9f21593f0036
seolyu <seolyu.90@gmail.com>
2019-07-09 13:44:22 +0900

    startcamp | learn | 190709

---
+++ 00_startcamp/day2/web_browser.py
@@ -0,0 +1,13 @@
+import webbrowser
+
+urls = [
+    'www.github.com',
+    'www.google.com',
+    'https://music.youtube.com',
+    'https://comic.naver.com',
+    'http://edu.ssafy.com'
+
+]
+
+for url in urls:
+    webbrowser.open(url)

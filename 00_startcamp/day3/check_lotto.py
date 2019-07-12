b29ec48134775611659da7d4a6231e00cf1742bf
seolyu <seolyu.90@gmail.com>
2019-07-12 10:26:21 +0900

    python | check_lotto1 | 190712

--- 00_startcamp/day3/check_lotto.py
+++ 00_startcamp/day3/check_lotto.py
@@ -1,7 +1,40 @@
 my = [1, 2, 3, 4, 5, 6]
 
 real = [1, 2, 3, 4, 5, 7]
-bonus = 6
+bonus = 8
+
+match_count = 0
+is_bonus = False
+
+for i in my:
+
+    if i == bonus:
+        is_bonus = True
+
+    for j in real:
+        if i == j:
+            # match_count = match_count + 1
+            match_count += 1
+
+print(match_count, is_bonus)
+        
+if match_count == 6:
+    result = '1등' #print는 디버깅용(값확인. 코드 종결X)
+elif match_count == 5:
+    #if is_bonus == True:
+    if is_bonus:
+        result = '2등'
+    else:
+        result = '3등'
+elif match_count == 4:
+    result = '4등'
+elif match_count == 3:
+    result = '5등'
+else:
+    result = '꽝ㅠ'
+
+print(result)
+
 
 # my와 real의 6개가 같으면 1등
 # my와 real이 5개가 같고 나머지 하나가 bonus면 2등
@@ -9,6 +42,7 @@ bonus = 6
 # '' 4개가 같으면 4등
 # '' 3개가 같으면 5등
 # 나머지는 꽝
+
 
 
 s2s3_student1 = {

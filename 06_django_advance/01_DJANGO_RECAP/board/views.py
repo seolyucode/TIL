7788a19371ce602dd60eeacb7394880257564c15
SeolYu, Lee <seolyu.90@gmail.com>
2019-10-07 13:55:16 +0900

    implement Read with MTV

--- 06_django_advance/01_DJANGO_RECAP/board/views.py
+++ 06_django_advance/01_DJANGO_RECAP/board/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
-
+from .models import Article
 
 def index(request):
     return render(request, 'board/index.html')
+
+
+def list(request):
+    articles = Article.objects.all()  # [<A1>, <A2>, <A3>, ...] 이런 식으로 return
+    return render(request, 'board/list.html', {
+        'articles': articles,
+    })
+
+
+def detail(request, id):
+    article = Article.objects.get(id=id)  # <A_id>
+    return render(request, 'board/detail.html', {
+        'article': article,
+    })

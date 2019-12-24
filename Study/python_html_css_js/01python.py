806383e228af99d0e105025b31aeb3eefaf75ea7
SeolYu Lee <seolyu.90@gmail.com>
2019-12-24 16:58:15 +0900

    20191224

---
+++ Study/python_html_css_js/01python.py
@@ -0,0 +1,23 @@
+x = 4
+y = 10
+print(x + y)
+
+# hello world
+'''
+hello world
+'''
+
+s = 'CEO SeolYu'
+print(type(s))
+print(dir(s))
+print(s[0:1])
+print(s[0:15:2])
+print(s[10:0:-1])
+print(s[::2])
+print(s[::-1])
+print(s[-1])
+print(s.upper())
+print(s.lower())
+ss = '    hello world    '
+print(ss.strip())
+print(s.split(' '))

84723eabbe18dbd879874856d46d928eba721b8e
SeolYu, Lee <seolyu.90@gmail.com>
2020-02-12 23:36:00 +0900

    props

--- Study/Vue_JS/my-project/new-cli-project/src/App.vue
+++ Study/Vue_JS/my-project/new-cli-project/src/App.vue
@@ -1,32 +1,25 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<div>
+  <h1>{{ title }}</h1>
+  <p>{{ count }}</p>
+  <button @click="count ++">추가</button>
+  <HomeComponent></HomeComponent>
+  <AppStatus></AppStatus>
+</div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+  import HomeComponent from './Home'
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  export default {
+    components: {
+      HomeComponent
+    },
+    data() {
+      return {
+        title: "안녕하세요",
+        count: 1
+      }
+    }
+  }
+</script>

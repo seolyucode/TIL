939c768145b066f4c97b018b1acb754d2a72bd86
SeolYu, Lee <seolyu.90@gmail.com>
2020-02-25 23:36:04 +0900

    3D scroll stage

---
+++ interactiveWebDevelopment/3DScroll/js/wall3d.js
@@ -0,0 +1,41 @@
+(function () {
+    const stageElem = document.querySelector('.stage');
+    const houseElem = document.querySelector('.house');
+    const barElem = document.querySelector('.progress-bar');
+    const mousePos = { x: 0, y: 0 };
+    // 전체 문서의 높이 - 윈도우의 높이(현재 창 사이즈)
+    // 스크롤 할 수 있는 범위
+    let maxScrollValue;
+
+    // 바뀐 창 사이즈에 따라
+    function resizeHandler() {
+        maxScrollValue = document.body.offsetHeight - window.innerHeight;
+    }
+
+    window.addEventListener('scroll', function () {
+        // console.log(pageYOffset);
+        // console.log(maxScrollValue);
+
+        // 얼마나 스크롤 했는지 비율
+        // console.log(pageYOffset / maxScrollValue);
+
+        const scrollPer = pageYOffset / maxScrollValue;
+        const zMove = scrollPer * 980 - 490;
+        houseElem.style.transform = 'translateZ(' + zMove + 'vw)';
+
+        // progress bar
+        barElem.style.width = scrollPer * 100 + '%';
+    });
+
+    window.addEventListener('mousemove', function(e) {
+        // console.log(e.clientX, e.clientY);
+        mousePos.x = -1 + (e.clientX / window.innerWidth) * 2;
+        mousePos.y = 1 - (e.clientY / window.innerHeight) * 2;
+        // console.log(mousePos);
+        stageElem.style.transform = 'rotateX(' + (mousePos.y * 5) + 'deg) rotateY(' + (mousePos.x * 5) + 'deg)';
+    });
+
+    // 바뀐 창 사이즈에 따라
+    window.addEventListener('resize', resizeHandler);
+    resizeHandler();
+})();

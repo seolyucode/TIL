a9ee129697339a2703d98d39dd5ab74bd6020a31
SeolYu, Lee <seolyu.90@gmail.com>
2020-03-04 13:32:38 +0900

    javaScript

---
+++ interactiveWebWithJavaScript/04코드의흐름II/array2.js
@@ -0,0 +1,16 @@
+var arr1 = [[2, 1], [4, 3]];
+var arr2 = [[3, 2, 1], [5, 4], [6]];
+var arr3 = [[3], [4, 5], [6], [7], [8, 9, 10]];
+
+function restoreArray(arr) {
+    var returnArr = [];
+
+    // 코드를 작성해주세요.
+    
+  
+    return returnArr;
+}
+
+console.log(restoreArray(arr1));
+console.log(restoreArray(arr2));
+console.log(restoreArray(arr3));

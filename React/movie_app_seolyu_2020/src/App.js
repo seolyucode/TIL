fc0c4029e4f602073570e973abf3579c2c6427f8
SeolYu, Lee <seolyu.90@gmail.com>
2020-03-22 21:56:05 +0900

    JSX Props

--- React/movie_app_seolyu_2020/src/App.js
+++ React/movie_app_seolyu_2020/src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+function Food({ name, picture }) {
+  return <div>
+    <h2>I like {name}</h2>
+    <img src={picture} />
+  </div>;
+}
+
+const foodILike = [
+  {
+    name: "Kimchi",
+    image:
+    "https://cdn.pixabay.com/photo/2020/01/01/00/15/one-address-based-4732816_1280.jpg"    
+  },
+  {
+    name: "Samgyeopsal",
+    image:
+    "https://cdn.pixabay.com/photo/2014/01/24/04/36/roasted-250875_1280.jpg"
+  }
+]
 
 function App() {
+  // return <div className="App" />
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodILike.map(dish => (
+        <Food name={dish.name} picture={dish.image}/>
+      ))}
     </div>
   );
 }

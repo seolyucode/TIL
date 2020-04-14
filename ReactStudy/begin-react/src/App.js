98ac6582006f2bda6c01880779881f081766fd40
SeolYu, Lee <seolyu.90@gmail.com>
2020-04-14 20:23:53 +0900

    useRef로 컴포넌트 안 변수 만들기 | 배열에 항목 추가

--- ReactStudy/begin-react/src/App.js
+++ ReactStudy/begin-react/src/App.js
@@ -1,13 +1,65 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import Hello from './Hello';
 import Wrapper from './Wrapper';
 import Counter from './Counter';
 import InputSample from './InputSample';
 import InputSample2 from './InputSample2';
 import UserList from './UserList';
+import CreateUser from './CreateUser';
 
 
 function App() {
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  });
+
+  const { username, email } = inputs;
+  const onChange = e => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+
+  const [users, setUsers] = useState([
+    {
+        id: 1,
+        username: 'Seolyu',
+        email: 'seolyu.90@gmail.com'
+    },
+    {
+        id: 2,
+        username: 'tester',
+        email: 'tester@example.com'
+    },
+    {
+        id: 3,
+        username: 'liz',
+        email: 'liz@example.com'
+    }
+  ]);
+
+  const nextId = useRef(4);
+
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    // setUsers([...users, user]);
+    setUsers(users.concat(user));
+    setInputs({
+      username: '',
+      email: ''
+    });
+
+    console.log(nextId.current);  // 4
+    nextId.current += 1;
+  }
+
   return (
     <Wrapper>
       {/* <Hello name="react" color="red" isSpecial={true} /> */}
@@ -17,8 +69,14 @@ function App() {
       <Counter />
       <InputSample />
       <InputSample2 />
-      
-      <UserList />
+
+      <CreateUser 
+        username={username} 
+        email={email} 
+        onChange={onChange} 
+        onCreate={onCreate} 
+      />
+      <UserList users={users}/>
     </Wrapper>
   )
 }
--- ReactStudy/begin-react/src/UserList.js
+++ ReactStudy/begin-react/src/UserList.js
@@ -8,24 +8,24 @@ function User({ user }) {
     );
 }
 
-function UserList() {
-    const users = [
-        {
-            id: 1,
-            username: 'Seolyu',
-            email: 'seolyu.90@gmail.com'
-        },
-        {
-            id: 2,
-            username: 'tester',
-            email: 'tester@example.com'
-        },
-        {
-            id: 3,
-            username: 'liz',
-            email: 'liz@example.com'
-        }
-    ];
+function UserList({ users }) {
+    // const users = [
+    //     {
+    //         id: 1,
+    //         username: 'Seolyu',
+    //         email: 'seolyu.90@gmail.com'
+    //     },
+    //     {
+    //         id: 2,
+    //         username: 'tester',
+    //         email: 'tester@example.com'
+    //     },
+    //     {
+    //         id: 3,
+    //         username: 'liz',
+    //         email: 'liz@example.com'
+    //     }
+    // ];
 
     return (
         <div>

20684a40352317db771a9a1e194fe4791eee08ab
SeolYu, Lee <seolyu.90@gmail.com>
2020-04-14 14:34:52 +0900

    input 상태 관리

--- ReactStudy/begin-react/src/App.js
+++ ReactStudy/begin-react/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Hello from './Hello';
 import Wrapper from './Wrapper';
 import Counter from './Counter';
+import InputSample from './InputSample';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hello color="pink" />
 
       <Counter />
+      <InputSample />
     </Wrapper>
   )
 }

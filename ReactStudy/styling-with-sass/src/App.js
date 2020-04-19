56fcb174d35a0350fc03e3c916133ed57cb7a752
SeolYu, Lee <seolyu.90@gmail.com>
2020-04-19 16:17:25 +0900

    Sass | size props | color props

--- ReactStudy/styling-with-sass/src/App.js
+++ ReactStudy/styling-with-sass/src/App.js
@@ -6,7 +6,19 @@ function App() {
   return (
     <div className="App">
       <div className="buttons">
-        <Button>Button</Button>
+        <Button size="large">BUTTON</Button>
+        <Button>BUTTON</Button>
+        <Button size="small">BUTTON</Button>
+      </div>
+      <div className="buttons">
+        <Button color="gray" size="large">BUTTON</Button>
+        <Button color="gray">BUTTON</Button>
+        <Button color="gray" size="small">BUTTON</Button>
+      </div>
+      <div className="buttons">
+        <Button color="pink" size="large">BUTTON</Button>
+        <Button color="pink">BUTTON</Button>
+        <Button color="pink" size="small">BUTTON</Button>
       </div>
     </div>
   );
--- ReactStudy/styling-with-sass/src/components/Button.js
+++ ReactStudy/styling-with-sass/src/components/Button.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import classNames from 'classnames';
+
 import './Button.scss';
 
-function Button({children}) {
-    return <button className="Button">{children}</button>
+// size: large, medium, small
+function Button({ children, size, color }) {
+    return <button className={classNames('Button', size, color)}>{children}</button>
 }
+
+Button.defaultProps = {
+    size: 'medium',
+    color: 'blue',
+};
 
 export default Button;

fe230bce0f6ca19d1e4268c008a5e3ac248929a0
SeolYu <seolyu.90@gmail.com>
2020-04-20 02:35:24 +0900

    기능 구현

--- ReactStudy/mashup-todolist/src/components/TodoCreate.js
+++ ReactStudy/mashup-todolist/src/components/TodoCreate.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import styled, { css } from 'styled-components';
 import { MdAdd } from 'react-icons/md';
+import { useTodoNextId, useTodoDispatch } from '../TodoContext';
 
 const CircleButton = styled.button`
     background: #38d919;
@@ -51,7 +52,7 @@ const InsertFormPositioner = styled.div`
     position: absolute;
 `;
 
-const InsertForm = styled.div`
+const InsertForm = styled.form`
     background: #f8f9fa;
     padding: 32px;
     padding-bottom: 72px;
@@ -72,14 +73,39 @@ const Input = styled.input`
 
 function TodoCreate() {
     const [open, setOpen] = useState(false);
+    const [value, setValue] = useState('');
+    const dispatch = useTodoDispatch();
+    const nextId = useTodoNextId();
+
     const onToggle = () => setOpen(!open);
+    const onChange = (e) => setValue(e.target.value);
+    const onSubmit = e => {
+        e.preventDefault();
+        dispatch({
+            type: 'CREATE',
+            todo: {
+                id: nextId.current,
+                text: value,
+                done: false
+            }
+        });
+        setValue('');
+        setOpen(false);
+        nextId.current += 1;
+    };
+
+
 
     return (
         <>
             {open && (
                 <InsertFormPositioner>
-                    <InsertForm>
-                        <Input placeholder="할 일을 입력 후, Enter 를 누르세요" autoFocus />
+                    <InsertForm onSubmit={onSubmit}>
+                        <Input placeholder="할 일을 입력 후, Enter 를 누르세요" 
+                        autoFocus 
+                        onChange={onChange} 
+                        value={value}
+                    />
                     </InsertForm>
                 </InsertFormPositioner>
             )}
@@ -90,4 +116,4 @@ function TodoCreate() {
     );
 }
 
-export default TodoCreate;
+export default React.memo(TodoCreate);

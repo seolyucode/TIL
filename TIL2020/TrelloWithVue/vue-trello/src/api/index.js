2c915ef58627514cbfa4dfe2ee2feac6590f4b55
SeolYu, Lee <seolyu.90@gmail.com>
2020-08-17 15:27:28 +0900

    로그인

--- TIL2020/TrelloWithVue/vue-trello/src/api/index.js
+++ TIL2020/TrelloWithVue/vue-trello/src/api/index.js
@@ -4,7 +4,7 @@ import router from '../router'
 const DOMAIN = 'https://server-msvml.run.goorm.io'
 const UNAUTHORIZED = 401
 const onUNAUTHORIZED = () => {
-    router.push('/login')
+    router.push(`/login?rPath=${encodeURIComponent(location.pathname)}`)
 }
 
 const request = (method, url, data) => {
@@ -15,13 +15,23 @@ const request = (method, url, data) => {
     }).then(result => result.data)
     .catch(result => {
         const {status} = result.response
-        if (status === UNAUTHORIZED) return onUNAUTHORIZED()
-        throw Error(result)
+        if (status === UNAUTHORIZED) onUNAUTHORIZED()
+        throw result.response
     })
+}
+
+export const setAuthInHeader = token => {
+    axios.defaults.headers.common['Authorization'] = token ? `Bearer ${token}` : null;
 }
 
 export const board = {
     fetch() {
         return request('get', '/boards')
+    }
+}
+
+export const auth = {
+    login(email, password) {
+        return request('post', '/login', {email, password})
     }
 }

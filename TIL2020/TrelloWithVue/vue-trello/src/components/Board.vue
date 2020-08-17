61419850bd503be30641daa8cf9099032e4e65d9
SeolYu, Lee <seolyu.90@gmail.com>
2020-08-17 14:07:05 +0900

    데이터 불러오기

--- TIL2020/TrelloWithVue/vue-trello/src/components/Board.vue
+++ TIL2020/TrelloWithVue/vue-trello/src/components/Board.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
-      Board
-      <div>bid: {{bid}}</div>
-      <router-link :to="`/b/${bid}/c/1`">Card 1</router-link>
-      <router-link :to="`/b/${bid}/c/2`">Card 2</router-link>
-      <hr />
-      <router-view></router-view>
+    Board
+    <div v-if="loading">loading board ...</div>
+    <div v-else>
+        <div>bid: {{bid}}</div>
+        <router-link :to="`/b/${bid}/c/1`">Card 1</router-link>
+        <router-link :to="`/b/${bid}/c/2`">Card 2</router-link>
+    </div>  
+    <hr />   
+    <router-view></router-view>
   </div>
 </template>
 
@@ -13,11 +16,21 @@
 export default {
     data() {
         return {
-            bid: 0
+            bid: 0,
+            loading: true
         }
     },
     created() {
-        this.bid = this.$route.params.bid
+        this.fetchData()
+    },
+    methods: {
+        fetchData() {
+            this.loading = true
+            setTimeout(() => {
+                this.bid = this.$route.params.bid
+                this.loading = false
+            }, 500)
+        }
     }
 }
 </script>

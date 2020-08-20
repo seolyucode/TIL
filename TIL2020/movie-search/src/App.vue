0131b197ab2f3d55cd4f8873a8f128b1f3f5f605
Seolyu ♪ *҉＼( ˆᴗˆ )ﾉ✩ <seolyu.90@gmail.com>
2020-08-21 01:52:19 +0900

    vue

---
+++ TIL2020/movie-search/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <search-bar />
+    <movie-list />
+  </v-app>
+</template>
+
+<script>
+import SearchBar from '@/components/SearchBar'
+import MovieList from '@/components/MovieList'
+
+export default {
+  components: {
+    SearchBar,
+    MovieList
+  }
+};
+</script>
---
+++ TIL2020/vue-movie-app/src/components/SearchBar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <v-text-field
+            v-model="title">
+
+        </v-text-field>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            title: ''
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>

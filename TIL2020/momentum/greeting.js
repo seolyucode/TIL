d14b090b4cf32d699c6d0bb3c9a178b481c6dad0
Seolyu ♪ *҉＼( ˆᴗˆ )ﾉ✩ <seolyu.90@gmail.com>
2020-09-25 05:47:22 +0900

    todo

--- TIL2020/momentum/greeting.js
+++ TIL2020/momentum/greeting.js
@@ -13,6 +13,7 @@ function handleSubmit(event) {
   event.preventDefault();
   const currentValue = input.value;
   paintGreeting(currentValue);
+  saveName(currentValue);
 }
 
 function askForName() {
